a77e03013a00f4e3b0c237ede6543b648d140932
lilliputten <lilliputten@yandex.ru>
2019-01-25 21:50:25 +0300

    Compatibility: bitbucket-hook.php

--- bitbucket-hook.php
+++ bitbucket-hook.php
@@ -1,37 +1,4 @@
 <?php
-/**
- * @module bitbucket-hook
- * @version 2018.10.21, 02:50
- *
- * Bitbucket webhook interface.
- *
- * Based on 'Automated git deployment' script by Jonathan Nicoal:
- * http://jonathannicol.com/blog/2013/11/19/automated-git-deployments-from-bitbucket/
- *
- * See README.md and config.sample.php
- *
- * ---
- * Igor Lilliputten
- * mailto: igor at lilliputten dot ru
- * http://lilliputten.ru/
- *
- * Ivan Pushkin
- * mailto: iv dot pushk at gmail dot com
- */
 
-// Initalize:
-require_once('log.php');
-require_once('bitbucket.php');
-
-// Load config:
-include('config.php');
-
-// Let's go:
-initConfig(); // Initialize repo configs
-initLog(); // Initialize log variables
-initPayload(); // Get posted data
-fetchParams(); // Get parameters from bitbucket payload (REPO)
-checkPaths(); // Check repository and project paths; create them if necessary
-placeVerboseInfo(); // Place verbose log information if specified in config
-fetchRepository(); // Fetch or clone repository
-checkoutProject(); // Checkout project into target folder
+// Compatibility mode with 'pre-github' (before 0.3.0) versions
+include('webhook.php');
